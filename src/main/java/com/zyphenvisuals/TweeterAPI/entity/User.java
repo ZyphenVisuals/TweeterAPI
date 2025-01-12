589559fdd5d7195500e2bb8032587e82840d2e5c
ZyphenVisuals <rotarujunior@gmail.com>
2025-01-12 19:00:34 +0200

    simplify User using lombok

--- src/main/java/com/zyphenvisuals/TweeterAPI/entity/User.java
+++ src/main/java/com/zyphenvisuals/TweeterAPI/entity/User.java
@@ -1,11 +1,12 @@
 package com.zyphenvisuals.TweeterAPI.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users", schema = "public")
 public class User {
@@ -15,15 +16,10 @@ public class User {
     private int id;
 
     @Column(unique = true, nullable = false)
+    @NonNull
     private String username;
 
     @Column(nullable = false, columnDefinition = "bpchar")
+    @NonNull
     private String password;
-
-    public User(String username, String hashedPassword) {
-        this.username = username;
-        this.password = hashedPassword;
-    }
-
-    public User() {}
 }
