51df3c63dc1645a997d98daef0ef2990527ad8ac
ZyphenVisuals <rotarujunior@gmail.com>
2025-01-13 21:31:08 +0200

    bump version

--- src/main/java/com/zyphenvisuals/TweeterAPI/entity/User.java
+++ src/main/java/com/zyphenvisuals/TweeterAPI/entity/User.java
@@ -1,7 +1,6 @@
 package com.zyphenvisuals.TweeterAPI.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
